d008d264b96379116b856570d0f2bebe9f88d264
Chris Williams <chris35williams@btinternet.com>
2017-07-14 15:46:23 +0100

    comment

--- scripts/example_aoi_run.sh
+++ scripts/example_aoi_run.sh
@@ -7,28 +7,28 @@ echo "All driver scripts are located in synthetic_channel_mesh/scripts/"
 python d01.py               
 
 ## Run s02_centreline_normal_development_NOT_EQUIDISTANT_smoothing.R
-Rscript d02.r                
-
-## Run s03_restructure_data.py
-python d03.py               
-
-## Run s04_mask_clip.r
-Rscript d04.r                
-
-## Run s05_get_channel_bank_elevation.py
-python d05.py               
-
-## Run s06_channel_parabola_EDGE_ELEVATIONS_piecewise.r
-Rscript d06.r                
-
-## Run s07_point_to_raster.r
-Rscript d07.r                
-
-## Run s08_get_minimum_surface_from_stack.r
-Rscript d08.r                
-
-## Run s09_combine_and_interp.r
-Rscript d09.r
+# Rscript d02.r                
+# 
+# ## Run s03_restructure_data.py
+# python d03.py               
+# 
+# ## Run s04_mask_clip.r
+# Rscript d04.r                
+# 
+# ## Run s05_get_channel_bank_elevation.py
+# python d05.py               
+# 
+# ## Run s06_channel_parabola_EDGE_ELEVATIONS_piecewise.r
+# Rscript d06.r                
+# 
+# ## Run s07_point_to_raster.r
+# Rscript d07.r                
+# 
+# ## Run s08_get_minimum_surface_from_stack.r
+# Rscript d08.r                
+# 
+# ## Run s09_combine_and_interp.r
+# Rscript d09.r
 
 
 
